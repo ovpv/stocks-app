27a3a331c87ba70b467aab24972c6bef1ef4437a
ovpv <viktor.prasad@gmail.com>
2019-05-07 23:52:11 +0530

    ::construction: connect socket and get data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { Component } from "react";
+import "./App.scss";
+import ConfigData from "./config";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	socket;
+	constructor() {
+		super();
+		this.state = {
+			data: ""
+		};
+	}
+	componentDidMount() {
+		this.socket = new WebSocket(ConfigData.url);
+		this.socket.onmessage = event => {
+			this.setState({
+				data: JSON.parse(event.data)
+			});
+		};
+	}
+	render() {
+		console.log(this.state.data);
+		return (
+			<div className="App">
+				<h2>Hello world</h2>
+			</div>
+		);
+	}
 }
-
-export default App;
