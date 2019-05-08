61f3b0cce7de3d1fd5bce20e30826b39b0ff536c
vishnuprasad <vishnuprasad.oduvathody@wisdmlabs.com>
2019-05-08 15:12:13 +0530

    :construction: convet stock array of arrays to unique object and save to state

--- src/App.js
+++ src/App.js
@@ -3,27 +3,36 @@ import "./App.scss";
 import ConfigData from "./config";
 
 export default class App extends Component {
-	socket;
-	constructor() {
-		super();
-		this.state = {
-			data: ""
-		};
-	}
-	componentDidMount() {
-		this.socket = new WebSocket(ConfigData.url);
-		this.socket.onmessage = event => {
-			this.setState({
-				data: JSON.parse(event.data)
-			});
-		};
-	}
-	render() {
-		console.log(this.state.data);
-		return (
-			<div className="App">
-				<h2>Hello world</h2>
-			</div>
-		);
-	}
+  socket;
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+  componentDidMount() {
+    this.socket = new WebSocket(ConfigData.url);
+    this.socket.onmessage = event => {
+      let Data = JSON.parse(event.data);
+      Data.forEach(([name, price]) => {
+        this.setState(prevstate => ({
+          data: { ...prevstate.data, [name]: price }
+        }));
+      });
+    };
+  }
+  render() {
+    const { data } = this.state;
+    return (
+      <div className="App">
+        <h2>Hello world</h2>
+        {Object.keys(data).map((key, index) => (
+          <div key={index}>
+            <div>{key}</div>
+            <div>{data[key]}</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
