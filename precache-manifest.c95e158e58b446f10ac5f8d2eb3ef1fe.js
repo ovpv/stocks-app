1fd378ec0696ed27de4192dd1f1babb5d8a9d988
ovpv <viktor.prasad@gmail.com>
2019-05-11 23:36:28 +0530

    Updates

---
+++ precache-manifest.c95e158e58b446f10ac5f8d2eb3ef1fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd5252047ad9db4e6a2e48129abf82f",
+    "url": "/stocks-app/index.html"
+  },
+  {
+    "revision": "0650e1c75c8c3b42b9b0",
+    "url": "/stocks-app/static/css/main.f0cdc0e2.chunk.css"
+  },
+  {
+    "revision": "eeee9c5ec9f2e170d402",
+    "url": "/stocks-app/static/js/2.f31b94f8.chunk.js"
+  },
+  {
+    "revision": "0650e1c75c8c3b42b9b0",
+    "url": "/stocks-app/static/js/main.8b225bcc.chunk.js"
+  },
+  {
+    "revision": "eac4764bebc67f9b7814",
+    "url": "/stocks-app/static/js/runtime~main.6feacc1b.js"
+  },
+  {
+    "revision": "0bb70c9dfe7ceed7a11577d2e22f9af8",
+    "url": "/stocks-app/static/media/down.0bb70c9d.png"
+  },
+  {
+    "revision": "ff158f209698afafa1a96727a90da2b6",
+    "url": "/stocks-app/static/media/skeleton.ff158f20.gif"
+  },
+  {
+    "revision": "7420f2b2ae10bd03befdb5e46401cb01",
+    "url": "/stocks-app/static/media/up.7420f2b2.png"
+  }
+]);
