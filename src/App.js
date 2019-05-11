57d81d82e9a036356b79e02911e93ed93548c1ac
ovpv <viktor.prasad@gmail.com>
2019-05-11 13:12:49 +0530

    :lipstick: add material table for displaying data. :heavy_plus_sign: add material ui core and icons packages

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
 import React, { Component } from "react";
 import "./App.scss";
 import ConfigData from "./config";
-
+import DataTable from "./components/table";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
 export default class App extends Component {
-  socket;
-  constructor() {
-    super();
-    this.state = {
-      data: []
-    };
-  }
-  componentDidMount() {
-    this.socket = new WebSocket(ConfigData.url);
-    this.socket.onmessage = event => {
-      let Data = JSON.parse(event.data);
-      Data.forEach(([name, price]) => {
-        this.setState(prevstate => ({
-          data: { ...prevstate.data, [name]: price }
-        }));
-      });
-    };
-  }
-  render() {
-    const { data } = this.state;
-    return (
-      <div className="App">
-        <h2>Hello world</h2>
-        {Object.keys(data).map((key, index) => (
-          <div key={index}>
-            <div>{key}</div>
-            <div>{data[key]}</div>
-          </div>
-        ))}
-      </div>
-    );
-  }
+	socket;
+	constructor() {
+		super();
+		this.state = {
+			data: []
+		};
+	}
+	componentDidMount() {
+		this.socket = new WebSocket(ConfigData.url);
+		this.socket.onmessage = event => {
+			let Data = JSON.parse(event.data);
+			Data.forEach(([name, price]) => {
+				this.setState(prevstate => ({
+					data: { ...prevstate.data, [name]: price }
+				}));
+			});
+		};
+	}
+	render() {
+		const { data } = this.state;
+		return (
+			<div className="App">
+				<h2>Hello world</h2>
+				<DataTable>
+					{Object.keys(data).map((key, index) => (
+						<TableRow key={index} count={Object.keys(data).length}>
+							<TableCell component="th" scope="row">
+								{key}
+							</TableCell>
+							<TableCell align="right">{data[key]}</TableCell>
+						</TableRow>
+					))}
+				</DataTable>
+			</div>
+		);
+	}
 }
